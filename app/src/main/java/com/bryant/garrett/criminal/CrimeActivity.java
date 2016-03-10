879628eaec7f155a9e913ba6515316466812f838
Garrett Bryant <goofy70geb@gmail.com>
2016-03-10 14:07:06 -0500

    Modifying details in the details view now update in the list view

--- app/src/main/java/com/bryant/garrett/criminal/CrimeActivity.java
+++ app/src/main/java/com/bryant/garrett/criminal/CrimeActivity.java
@@ -7,7 +7,7 @@ import android.support.v4.app.Fragment;
 import java.util.UUID;
 
 public class CrimeActivity extends SingleFragmentActivity {
-    public static final String EXTRA_CRIME_ID = "com.bryant.garrett.criminal.crime_id";
+    private static final String EXTRA_CRIME_ID = "com.bryant.garrett.criminal.crime_id";
 
     public static Intent newIntent(Context packageContext, UUID crimeId) {
         Intent intent = new Intent(packageContext, CrimeActivity.class);
@@ -17,7 +17,9 @@ public class CrimeActivity extends SingleFragmentActivity {
 
     @Override
      protected Fragment createFragment() {
-        return new CrimeFragment();
+        UUID crimeId = (UUID) getIntent()
+                .getSerializableExtra(EXTRA_CRIME_ID);
+        return CrimeFragment.newInstance(crimeId);
     }
 
 }
